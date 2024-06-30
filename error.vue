7b7f924e1f8a6a494e2ccf3a16de55fc2cb59876
pierreleripoll <pierreripoll13@gmail.com>
2024-06-30 19:48:03 +0200

    feat: refactor default layout + error page + stable gutter scrollbar

---
+++ error.vue
@@ -0,0 +1,18 @@
+<template>
+  <NuxtLayout>
+    <div class="error-page">
+      <h1>404</h1>
+      <p>La page que vous recherchez n'existe pas</p>
+      <NuxtLink to="/">Acceuil</NuxtLink>
+    </div>
+  </NuxtLayout>
+</template>
+
+<style scoped>
+.error-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,42 +6,34 @@
   <div class="gallery section">
     <div v-for="image in imageData" :key="image.id" class="image">
       <picture>
-        <!-- For WebP images -->
         <source
-          :srcset="`${basePath}/${image.id}-600px.webp 600w, ${basePath}/${image.id}-1080px.webp 1080w, ${basePath}/${image.id}-1920px.webp 1920w, ${basePath}/${image.id}-original.webp 2560w`"
+          :srcset="createSrcSet(image.id, 'webp')"
           sizes="(max-width: 1200px) 600px, (max-width: 1919px) 1080px, (max-width: 2500px) 1920px, 100vw"
           type="image/webp"
         />
-        <!-- For JPEG images -->
         <source
-          :srcset="`${basePath}/${image.id}-600px.jpeg 600w, ${basePath}/${image.id}-1080px.jpeg 1080w, ${basePath}/${image.id}-1920px.jpeg 1920w, ${basePath}/${image.id}-original.jpeg 2560w`"
+          :srcset="createSrcSet(image.id, 'jpeg')"
           sizes="(max-width: 1200px) 600px, (max-width: 1919px) 1080px, (max-width: 2500px) 1920px, 100vw"
           type="image/jpeg"
         />
-        <!-- Fallback IMG element for browsers that do not support <picture> -->
         <img
           :loading="Number(image.id) > 2 ? 'lazy' : 'eager'"
           decoding="async"
           class="responsive-img"
-          :width="image.width"
-          :height="image.height"
-          :src="`${basePath}/${image.id}-original.jpeg`"
+          :src="getImagePath(image.id, 'jpeg')"
           :alt="`Image ${image.id}`"
         />
       </picture>
     </div>
   </div>
   <div class="contact section">
-    <p>
-      <a href="mailto:martin.bruneau50@gmail.com" class="email-link"
-        >martin.bruneau50@gmail.com</a
-      ><br />
-      Lausanne - Paris <br />
-      +41 77 416 32 58 <br />
-      +33 6 61 03 54 20
-    </p>
+    <a href="mailto:martin.bruneau50@gmail.com" class="email-link"
+      >martin.bruneau50@gmail.com</a
+    ><br />
+    Lausanne - Paris<br />
+    +41 77 416 32 58<br />
+    +33 6 61 03 54 20
   </div>
-
   <div class="footer section">
     <p>Site web réalisé par Pierre Ripoll</p>
   </div>
@@ -49,44 +41,16 @@
 
 <script setup>
 import imageData from "@/assets/metadata-photos.json";
-
 const basePath = "https://martinbruneau.s3.sbg.io.cloud.ovh.net/optimized";
+
+const getImagePath = (id, type) => `${basePath}/${id}-original.${type}`;
+const createSrcSet = (id, type) => {
+  return [600, 1080, 1920, 2560]
+    .map((size) => `${basePath}/${id}-${size}px.${type} ${size}w`)
+    .join(", ");
+};
 </script>
-
 <style scoped>
-.container {
-  font-family: "Helvetica Neue", Arial, sans-serif;
-  margin-top: -8px;
-  padding: 1em 2em;
-  text-align: start;
-}
-
-.header {
-  justify-content: space-between;
-  max-width: 1500px;
-  text-align: start;
-  margin: 0 auto;
-  margin-bottom: 10vh;
-}
-
-.header h1 {
-  font-size: 36px;
-  font-weight: bold;
-  text-align: start;
-  flex-shrink: 1;
-}
-
-.header h2 {
-  font-size: 24px;
-  font-weight: normal;
-  margin: auto 0;
-  flex-shrink: 2;
-}
-
-.presentation {
-  font-size: 18px;
-}
-
 .gallery {
   display: flex;
   flex-direction: column;
@@ -96,50 +60,31 @@ const basePath = "https://martinbruneau.s3.sbg.io.cloud.ovh.net/optimized";
 }
 
 .image {
-  width: 100%; /* Full width to ensure picture element takes up the container width */
+  width: 100%;
   display: flex;
-  justify-content: center; /* Center images horizontally */
-  margin-bottom: 20px; /* Spacing between images */
+  justify-content: center;
+  margin-bottom: 20px;
 }
 
 .responsive-img {
   max-height: 85vh;
-  max-width: 100%; /* Ensure the image does not exceed its container's width */
-  height: auto; /* Maintain aspect ratio */
-  width: auto; /* Maintain aspect ratio */
+  max-width: 100%;
+  height: auto;
+  width: auto;
 }
 
-@media screen and (min-width: 500px) {
-  .header {
-    text-align: center;
-    position: sticky;
-    top: 0;
-    display: flex;
-    flex-direction: row;
-  }
-
-  .container {
-    padding: 0 30px;
-  }
-  .gallery {
-    padding: 0 20vw;
-  }
-
-  .presentation {
-    max-width: 40vw;
-  }
-}
-
-.section {
-  margin-bottom: 10vh;
+.presentation,
+.contact,
+.footer {
+  max-width: 40vw;
 }
 
 .email-link {
-  color: inherit; /* Keeps the color consistent with the surrounding text */
-  text-decoration: underline; /* Underlines the link */
+  color: inherit;
+  text-decoration: underline;
 }
 
 .email-link:hover {
-  text-decoration: none; /* Optional: removes underline on hover for a different effect */
+  text-decoration: none;
 }
 </style>
