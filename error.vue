1373d6ff831e4c6faedb9525758546d6ff77858a
pierreleripoll <pierreripoll13@gmail.com>
2024-06-30 20:34:29 +0200

    feat(error): add error status code + error message

--- error.vue
+++ error.vue
@@ -1,12 +1,25 @@
 <template>
   <NuxtLayout>
     <div class="error-page">
-      <h1>404</h1>
-      <p>La page que vous recherchez n'existe pas</p>
+      <h1>{{ error?.statusCode ?? "404" }}</h1>
+      <p>
+        {{ error?.message ?? "La page que vous recherchez n'existe pas" }}
+      </p>
       <NuxtLink to="/">Acceuil</NuxtLink>
     </div>
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+const props = defineProps({
+  error: Object as () => NuxtError,
+});
+console.error(props.error);
+
+const handleError = () => clearError({ redirect: "/" });
+</script>
 
 <style scoped>
 .error-page {
