4444744f81ed2e614a9bf77975f702016af1fe1b
pierreleripoll <pierreripoll13@gmail.com>
2024-07-05 22:20:59 +0200

    feat: added meta tags

--- app.vue
+++ app.vue
@@ -7,6 +7,17 @@
   </div>
 </template>
 
+<script setup lang="ts">
+useSeoMeta({
+  title: "Martin Bruneau",
+  ogTitle: "Martin Bruneau",
+  description:
+    "Aperçu d’une série de 64 scans et photographies collectées à Cherbourg et ses environs.",
+  ogDescription:
+    "Aperçu d’une série de 64 scans et photographies collectées à Cherbourg et ses environs.",
+});
+</script>
+
 <style>
 html {
   scrollbar-gutter: stable;
